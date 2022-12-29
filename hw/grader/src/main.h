e1a33c7aa327a98e758101a6110af307cebcec55
Kaloyan Doychinov <kaloyan@kaloyan.tech>
2022-12-29 20:56:28 +0200

    [FINISH] Dynamic Version

--- hw/grader/src/main.h
+++ hw/grader/src/main.h
@@ -11,10 +11,4 @@
 #define MAX_STUDENTS_DIGITS 4  // 2 digits + '\0' + '/
 #define MAX_STUDENTS 25
 
-// char *get_correct_answers(int afid);
-
-uint8_t get_points(int sfid, uint8_t *correct_answers);
-uint8_t get_grade(uint8_t points);
-void grade_student(char *path_to_student, char *path_to_result,
-                   char *correct_answers);
 void grader(int argc, char *argv[]);
